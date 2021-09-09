b7352fbd3252b274eb73fde60902fe26a6801d35
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-09-09 11:33:40 +0200

    JPQL Query

--- src/main/java/zw/co/afrosoft/studentapp/persistence/StudentRepository.java
+++ src/main/java/zw/co/afrosoft/studentapp/persistence/StudentRepository.java
@@ -1,6 +1,7 @@
 package zw.co.afrosoft.studentapp.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import zw.co.afrosoft.studentapp.domain.Student;
 
 import java.util.List;
@@ -11,4 +12,8 @@ public interface StudentRepository extends JpaRepository<Student,Long> {
     Student findByFirstNameAndLastName(String firstName, String lastName);
     List<Student> findByFirstNameContains(String firstName);
     List<Student> findByFirstNameStartsWith(String firstName);
+
+    //JPQL
+//    @Query("From Student where firstName = :firstName and lastName = : lastName")
+//    Student getByFirstNameAndLastName(String firstName, String lastName);
 }
