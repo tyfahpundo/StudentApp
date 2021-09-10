5ce71d17127419e4e10ca72a0d10d3d6e06b313b
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-09-10 13:54:20 +0200

    Created Address Entity

---
+++ src/main/java/zw/co/afrosoft/studentapp/domain/Address.java
@@ -0,0 +1,18 @@
+package zw.co.afrosoft.studentapp.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String city;
+}
